4252363d1d69b3cea02bb8fce65a77205c2cffc4
Katy Overstreet <katy@Jakes-MacBook-Pro.local>
2021-02-12 04:52:07 -0500

    added to js

---
+++ main.js
@@ -0,0 +1,40 @@
+//1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+/* these are the three variables that store references 
+to the name button, story button, and the <p> in the HTML*/
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+  }
+
+
+  let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+  
+  //storing values into the arrays
+  let insertX = ["Willy the Goblin, Big Daddy, Father Christmas"];
+  let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+  let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"] ;
+
+  randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    let name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300);
+    let temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
